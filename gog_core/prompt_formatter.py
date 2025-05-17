e481fcea1e5953d66a255fdca3e112a2b2241311
mounakrishna <mounakrishnarouthu@gmail.con>
2025-05-17 15:34:27 +0530

    added author

--- gog_core/prompt_formatter.py
+++ gog_core/prompt_formatter.py
@@ -2,9 +2,10 @@ def format_prompt(prompt_data):
     context = prompt_data.get("context", "")
     tone = prompt_data.get("tone", "casual")
     audience = prompt_data.get("audience", "general")
+    author = prompt_data.get("author", "general")
     fmt = prompt_data.get("format", "text")
     include_headings = prompt_data.get("include_headings", False)
 
     return (
-        f"Write about {context} in a {tone} tone for a {audience} audience in a {fmt} format.Include headings is {include_headings}"
+        f"Write about {context} in a {tone} tone for a {audience} audience , the audience is like who is hearing it, and author {author} is who is reading, in a {fmt} format.Include headings is {include_headings}"
     )
